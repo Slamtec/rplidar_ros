adfef59cd5bfb813427035aa03e43bf45de1b611
Hunter L. Allen <hunter.allen@ghostrobotics.io>
2020-07-23 10:24:21 -0400

    Fix building with GCC 10 (#17)
    Signed-off-by: Hunter L. Allen <hunter.allen@ghostrobotics.io>

--- sdk/src/rplidar_driver.cpp
+++ sdk/src/rplidar_driver.cpp
@@ -1821,7 +1821,7 @@ u_result RPlidarDriverImplCommon::grabScanData(rplidar_response_measurement_node
 {
     DEPRECATED_WARN("grabScanData()", "grabScanDataHq()");
 
-    switch (_dataEvt.wait(timeout))
+    switch (static_cast<int>(_dataEvt.wait(timeout)))
     {
     case rp::hal::Event::EVENT_TIMEOUT:
         count = 0;
@@ -1850,7 +1850,7 @@ u_result RPlidarDriverImplCommon::grabScanData(rplidar_response_measurement_node
 
 u_result RPlidarDriverImplCommon::grabScanDataHq(rplidar_response_measurement_node_hq_t * nodebuffer, size_t & count, _u32 timeout)
 {
-    switch (_dataEvt.wait(timeout))
+    switch (static_cast<int>(_dataEvt.wait(timeout)))
     {
     case rp::hal::Event::EVENT_TIMEOUT:
         count = 0;
