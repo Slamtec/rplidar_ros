0bb56aa1e611184b9c43d836ec9235bc419d9a81
Lewis Kaye <lewiskaye@hotmail.co.uk>
2022-09-28 21:22:22 +0100

    Adding types to declare_parameter functions
    Fixing a compile issue on Ubuntu 22.04 running ROS2 Humble.  
    Upon compilation of a fresh 'git clone', colcon build failed to compile the package, identifying missing type's from the "this->declare_parameter" functions in the initialisation section of the node.

--- src/rplidar_scan_publisher.cpp
+++ src/rplidar_scan_publisher.cpp
@@ -64,15 +64,15 @@ class RPLidarScanPublisher : public rclcpp::Node
   private:    
     void init_param()
     {
-        this->declare_parameter("channel_type");
-        this->declare_parameter("tcp_ip");
-        this->declare_parameter("tcp_port");
-        this->declare_parameter("serial_port");
-        this->declare_parameter("serial_baudrate");
-        this->declare_parameter("frame_id");
-        this->declare_parameter("inverted");
-        this->declare_parameter("angle_compensate");
-        this->declare_parameter("scan_mode");
+        this->declare_parameter<std::string>("channel_type");
+        this->declare_parameter<std::string>("tcp_ip");
+        this->declare_parameter<int>("tcp_port");
+        this->declare_parameter<std::string>("serial_port");
+        this->declare_parameter<int>("serial_baudrate");
+        this->declare_parameter<std::string>("frame_id");
+        this->declare_parameter<bool>("inverted");
+        this->declare_parameter<bool>("angle_compensate");
+        this->declare_parameter<std::string>("scan_mode");
 
         this->get_parameter_or<std::string>("channel_type", channel_type, "serial");
         this->get_parameter_or<std::string>("tcp_ip", tcp_ip, "192.168.0.7"); 
