57c3e68b9d58d89c1a9bf8233e302cdff4af6b2c
Julien Kindle <julien.kindle@gmail.com>
2023-03-29 16:02:56 +0000

    Adapting params for humble

--- src/rplidar_scan_publisher.cpp
+++ src/rplidar_scan_publisher.cpp
@@ -64,25 +64,25 @@ class RPLidarScanPublisher : public rclcpp::Node
   private:    
     void init_param()
     {
-        this->declare_parameter("channel_type");
-        this->declare_parameter("tcp_ip");
-        this->declare_parameter("tcp_port");
-        this->declare_parameter("serial_port");
-        this->declare_parameter("serial_baudrate");
-        this->declare_parameter("frame_id");
-        this->declare_parameter("inverted");
-        this->declare_parameter("angle_compensate");
-        this->declare_parameter("scan_mode");
-
-        this->get_parameter_or<std::string>("channel_type", channel_type, "serial");
-        this->get_parameter_or<std::string>("tcp_ip", tcp_ip, "192.168.0.7"); 
-        this->get_parameter_or<int>("tcp_port", tcp_port, 20108);
-        this->get_parameter_or<std::string>("serial_port", serial_port, "/dev/ttyUSB0"); 
-        this->get_parameter_or<int>("serial_baudrate", serial_baudrate, 115200/*256000*/);//ros run for A1 A2, change to 256000 if A3
-        this->get_parameter_or<std::string>("frame_id", frame_id, "laser_frame");
-        this->get_parameter_or<bool>("inverted", inverted, false);
-        this->get_parameter_or<bool>("angle_compensate", angle_compensate, false);
-        this->get_parameter_or<std::string>("scan_mode", scan_mode, std::string());
+        this->declare_parameter("channel_type", "serial");
+        this->declare_parameter("tcp_ip", "192.168.0.7");
+        this->declare_parameter("tcp_port", 20108);
+        this->declare_parameter("serial_port", "/dev/ttyUSB0");
+        this->declare_parameter("serial_baudrate", 115200); //ros run for A1 A2, change to 256000 if A3
+        this->declare_parameter("frame_id", "laser_frame");
+        this->declare_parameter("inverted", false);
+        this->declare_parameter("angle_compensate", false);
+        this->declare_parameter("scan_mode", "");
+        
+        this->get_parameter<std::string>("channel_type", channel_type);
+        this->get_parameter<std::string>("tcp_ip", tcp_ip); 
+        this->get_parameter<int>("tcp_port", tcp_port);
+        this->get_parameter<std::string>("serial_port", serial_port); 
+        this->get_parameter<int>("serial_baudrate", serial_baudrate);
+        this->get_parameter<std::string>("frame_id", frame_id);
+        this->get_parameter<bool>("inverted", inverted);
+        this->get_parameter<bool>("angle_compensate", angle_compensate);
+        this->get_parameter<std::string>("scan_mode", scan_mode);
     }
 
     bool getRPLIDARDeviceInfo(RPlidarDriver * drv)
