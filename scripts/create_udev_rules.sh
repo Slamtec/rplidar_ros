05fd0b543ca15773bfaae344e31626163703383c
haozhou.wong <haozhou.wong@gmail.com>
2023-05-17 01:25:06 +0800

    create_udev_rule.sh bug fix in ros2

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -3,7 +3,8 @@
 echo "remap the device serial port(ttyUSBX) to  rplidar"
 echo "rplidar usb connection as /dev/rplidar , check it using the command : ls -l /dev|grep ttyUSB"
 echo "start copy rplidar.rules to  /etc/udev/rules.d/"
-colcon_cd rplidar_ros2
+source /usr/share/colcon_cd/function/colcon_cd.sh
+colcon_cd rplidar_ros
 sudo cp scripts/rplidar.rules  /etc/udev/rules.d
 echo " "
 echo "Restarting udev"
