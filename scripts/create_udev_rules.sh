05bd8f0283945329fe5c34a108bc40807e2efebb
deyou wang <deyou.wang@slamtec.com>
2023-05-18 14:52:40 +0800

    Update create_udev_rules.sh and README.md

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-echo "remap the device serial port(ttyUSBX) to  rplidar"
-echo "rplidar usb connection as /dev/rplidar , check it using the command : ls -l /dev|grep ttyUSB"
-echo "start copy rplidar.rules to  /etc/udev/rules.d/"
+echo "remap the device serial port(ttyUSBX) to rplidar"
+echo "rplidar usb connection as /dev/rplidar, check it using the command : ls -l /dev | grep ttyUSB"
+echo "start copy rplidar.rules to /etc/udev/rules.d/"
 source /usr/share/colcon_cd/function/colcon_cd.sh
 colcon_cd rplidar_ros
 sudo cp scripts/rplidar.rules  /etc/udev/rules.d
-echo " "
-echo "Restarting udev"
-echo ""
-sudo service udev reload
-sudo service udev restart
-echo "finish "
+echo -e "\nRestarting udev\n"
+sudo systemctl restart udev
+sudo udevadm control --reload && sudo udevadm trigger
+echo "finish"
