890ab1ddf95403139ac9966f0d5ec9001e425340
deyou wang <deyou.wang@slamtec.com>
2023-06-21 18:55:41 +0800

    Update create_udev_rules.sh

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "remap the device serial port(ttyUSBX) to  rplidar"
-echo "rplidar usb connection as /dev/rplidar , check it using the command : ls -l /dev|grep ttyUSB"
-echo "start copy rplidar.rules to  /etc/udev/rules.d/"
-echo "`rospack find rplidar_ros`/scripts/rplidar.rules"
-sudo cp `rospack find rplidar_ros`/scripts/rplidar.rules  /etc/udev/rules.d
-echo " "
-echo "Restarting udev"
-echo ""
+echo "remap the device serial port(ttyUSBX) to rplidar"
+echo "rplidar usb connection as /dev/rplidar, check it using the command : ls -l /dev | grep ttyUSB"
+echo "start copy rplidar.rules to /etc/udev/rules.d/"
+source /usr/share/colcon_cd/function/colcon_cd.sh
+colcon_cd rplidar_ros
+sudo cp scripts/rplidar.rules  /etc/udev/rules.d
+echo -e "\nRestarting udev\n"
 sudo service udev reload
 sudo service udev restart
-echo "finish "
+sudo udevadm control --reload && sudo udevadm trigger
+echo "finish"
